094fb12df77110f56298fbb191669979f485792b
Paolo Gremi <gremipaolo@gmail.com>
2018-04-17 15:39:36 +0200

    abbiamo creato una cartella

---
+++ src/Tasti.java
@@ -0,0 +1,11 @@
+public class Tasti {
+    private Integer valore;
+
+    public Tasti(Integer valore) {
+        this.valore = valore;
+    }
+
+    public Integer getValore() {
+        return valore;
+    }
+}
