3ea82c44767dca6e1b11e08a1095a1f0c8fe244c
UTENTE <edda.grillo01@universitadipavia.it>
2018-04-24 16:03:10 +0200

    modifica costruttore con aggiunta di metodi getter

--- src/Phone.java
+++ src/Phone.java
@@ -1,11 +1,17 @@
 public class Phone {
     private String id;
+    private Button [] buttons;
 
-    public Phone(String id) {
+    public Phone(String id, Button buttons) {
         this.id = id;
+        this.buttons = new  Button[12];
     }
 
     public String getId() {
         return id;
+    }
+
+    public Button[] getButtons() {
+        return buttons;
     }
 }
