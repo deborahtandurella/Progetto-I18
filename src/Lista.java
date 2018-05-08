27b912d2c1500c1a9d9ece3d559bb9eb00ebd982
Paolo Gremi <gremipaolo@gmail.com>
2018-05-08 11:34:31 +0200

    prima implementazione delle liste di risposte

---
+++ src/Lista.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Lista {
+    private ArrayList<Answer> x;
+
+    public Lista(ArrayList<Answer> x) {
+        this.x = x;
+    }
+
+    public ArrayList<Answer> getX() {
+        return x;
+    }
+}
