2658adc8a782c0a3d7739033c682c74f73c9b3de
h0000ly <37170577+h0000ly@users.noreply.github.com>
2019-06-27 10:15:07 +0200

    fail delete

--- src/clientServer/MultiThreadClient.java
+++
@@ -1,9 +0,0 @@
-package clientServer;
-
-public class MultiThreadClient {
-    public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Client();
-        t1.start();
-        t1.join();
-    }
-}
--- src/clientServer/ServerInfo.java
+++
@@ -1,9 +0,0 @@
-package clientServer;
-
-/**
- * This class contains all the connection info to the server
- */
-public class ServerInfo {
-    public final static String IP = "127.0.0.1";
-    public final static int PORT = 8081;
-}
